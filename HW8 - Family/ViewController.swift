987748a329d1b0c7e4ae08797e2ed3efc16ca3b7
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-11-27 09:46:40 +0300

    Initial Commit

---
+++ HW8 - Family/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW8 - Family
+//
+//  Created by Сергей Ткаченко on 27.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
